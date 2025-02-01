61219e520976c5aa6d73361b73452c4c8ced0f09
Igor Smirnov <igor.smirnov@deltasolutions.ru>
2025-02-01 17:54:08 +0300

    Добавьте файлы проекта.

---
+++ Events/IncidentWorker_TestIndcident.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace GreenFight.Events
+{
+    public class IncidentWorker_TestIndcident : IncidentWorker
+    {
+        protected override bool CanFireNowSub(IncidentParms parms)
+        {
+            return testEvent(parms);
+        }
+
+        protected override bool TryExecuteWorker(IncidentParms parms)
+        {
+            return testEvent(parms);
+        }
+
+        private bool testEvent(IncidentParms parms)
+        {
+            Map map = (Map)parms.target;
+            int pawnsCount = map.mapPawns.ColonistsSpawnedCount;
+            if (pawnsCount < 5)
+            {
+                Log.Message($"Colonist count -> {pawnsCount}");
+                SendStandardLetter(def.letterLabel, def.letterText, LetterDefOf.ThreatBig, parms, null);
+                return false;
+            }
+            else
+            {
+                foreach (var pawn in map.mapPawns.FreeColonists)
+                {
+                    Log.Message($"Pawn -> {pawn.Name.ToStringFull}");
+                }
+
+                SendStandardLetter(def.letterLabel, def.letterText, LetterDefOf.NeutralEvent, parms, null);
+                return true;
+            }
+        }
+    }
+}
