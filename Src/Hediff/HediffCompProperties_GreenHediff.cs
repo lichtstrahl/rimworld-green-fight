cd561db354103bd5686d13b2a4b425c017ad19bd
Igor Smirnov <igor.smirnov@deltasolutions.ru>
2025-04-28 19:52:55 +0300

    Болезнь, которая периодически взрывает больного

---
+++ Src/Hediff/HediffCompProperties_GreenHediff.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+namespace GreenFight.Hediff
+{
+    public class HediffCompProperties_GreenHediff : HediffCompProperties
+    {
+        public float value;
+        
+        public HediffCompProperties_GreenHediff()
+        {
+            compClass = typeof(HediffComp_GreenHediff);
+        }
+    }
+}
---
+++ Src/Hediff/HediffComp_GreenHediff.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+using Verse;
+
+namespace GreenFight.Hediff
+{
+    public class HediffComp_GreenHediff : HediffComp
+    {
+        public HediffCompProperties_GreenHediff GreenProps => (HediffCompProperties_GreenHediff)props;
+
+        /**
+         * Обработка тика болезнью (конкретной его частью).
+         *
+         * severityAdjustment - изменение силы болезни за этот тик. Значение можно перезаписывать т.к. оно ref.
+         */
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            base.CompPostTick(ref severityAdjustment);
+
+            if (Pawn.IsHashIntervalTick(500))
+            {
+                GenExplosion.DoExplosion(Pawn.Position, Pawn.Map, GreenProps.value, DamageDefOf.Bomb, null);
+            }
+        }
+    }
+}
