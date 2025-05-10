20be3d78cd4be8d89c4c1dd8b0b069f89641bc41
Igor Smirnov <igor.smirnov@deltasolutions.ru>
2025-05-10 15:35:06 +0300

    Новое оружие с взрывающимися патронами

---
+++ Src/Bullet/Bullet_GreenBoomBullet.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using Verse;
+
+namespace GreenFight.Bullet
+{
+    public class Bullet_GreenBoomBullet : RimWorld.Bullet
+    {
+        protected override void Impact(Thing hitThing, bool blockedByShield = false)
+        {
+            base.Impact(hitThing, blockedByShield);
+
+            if (hitThing != null)
+            {
+                GenExplosion.DoExplosion(
+                    hitThing.Position,
+                    hitThing.Map,
+                    Rand.Range(1, 3),
+                    DamageDefOf.Bomb,
+                    launcher
+                );
+            }
+        }
+    }
+}
