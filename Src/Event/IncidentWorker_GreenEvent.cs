2ed78d40b8d811103c0af4927e438fe154145d12
Igor Smirnov <igor.smirnov@deltasolutions.ru>
2025-06-09 15:20:06 +0300

    Настройка мода

---
+++ Src/Event/IncidentWorker_GreenEvent.cs
@@ -0,0 +1,22 @@
+﻿using GreenFight.Mod;
+using RimWorld;
+using Verse;
+
+namespace GreenFight.Events
+{
+    public class IncidentWorker_GreenEvent : IncidentWorker
+    {
+        protected override bool TryExecuteWorker(IncidentParms parms)
+        {
+            Map map = parms.target as Map;
+            map.weatherManager.TransitionTo(GreenMod.Settings.WeatherDef);
+
+            IncidentParms raidParams = StorytellerUtility.DefaultParmsNow(IncidentDefOf.RaidEnemy.category, map);
+            raidParams.points = GreenMod.Settings.RaidPowerRange.RandomInRange;
+
+            IncidentDefOf.RaidEnemy.Worker.TryExecute(raidParams);
+            
+            return true;
+        }
+    }
+}
---
+++ Src/Mod/GreenModSettings.cs
@@ -0,0 +1,77 @@
+﻿using System.Linq;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace GreenFight.Mod
+{
+    public class GreenModSettings : ModSettings
+    {
+        private static string _LanguageKey = "GreenModSettings";
+        
+        private string _weatherDefName;
+        private WeatherDef _weather;
+
+        public WeatherDef WeatherDef
+        {
+            get
+            {
+                if (_weather == null)
+                {
+                    if (!_weatherDefName.NullOrEmpty())
+                    {
+                        _weather = DefDatabase<WeatherDef>.GetNamed(_weatherDefName);
+                    }
+                    else
+                    {
+                        _weather = WeatherDefOf.Clear;
+                    }
+                }
+                
+                return _weather;
+            }
+        }
+
+        public IntRange RaidPowerRange = new IntRange(100, 500);
+        private Vector2 _scrollPosition = Vector2.zero;
+        private float _viewHeight = 0;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look<string>(ref _weatherDefName, "weatherDefName");
+            Scribe_Values.Look<IntRange>(ref RaidPowerRange, "RaidPowerRange", new IntRange(100, 500));
+        }
+
+        public void DoSettingsWindowContents(Rect inRect)
+        {
+            Rect viewRect = new Rect(0, 0, inRect.width - 16f, _viewHeight);
+            Widgets.BeginScrollView(inRect, ref _scrollPosition, viewRect);
+
+            Listing_Standard listing = new Listing_Standard()
+            {
+                ColumnWidth = inRect.width
+            };
+            Rect listingRect = inRect.AtZero();
+            listingRect.height = 99_999f;
+            listing.Begin(listingRect);
+
+            if (listing.ButtonText($"{_LanguageKey}_SelectWeather".Translate(WeatherDef.LabelCap)))
+            {
+                var options = DefDatabase<WeatherDef>.AllDefsListForReading
+                    .Select(weather => new FloatMenuOption(weather.LabelCap, () =>
+                    {
+                        _weatherDefName = weather.defName;
+                        _weather = null;
+                    }));
+                Find.WindowStack.Add(new FloatMenu(options.ToList()));
+            }
+
+            listing.IntRange(ref RaidPowerRange, 1, 10_000);
+            
+            listing.End();
+            _viewHeight = listing.CurHeight;
+            
+            Widgets.EndScrollView();
+        }
+    }
+}
